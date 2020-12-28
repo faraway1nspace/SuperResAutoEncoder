6ef7763449899bf814fd3b872dfc1c2a0cc0350d
faraway1nspace <robertw.rankin@gmail.com>
2020-12-28 09:22:44 -0500

    first commit

---
+++ src/nets/transformer_net.py
@@ -0,0 +1,271 @@
+import torch
+
+class HyperParams:
+    def __init__(self, params_dict):
+        for k,v in params_dict.items():
+            self.__setattr__(k,v)
+
+class TransformerNet(torch.nn.Module):
+    def __init__(self):
+        super(TransformerNet, self).__init__()
+        # Initial convolution layers
+        self.conv1 = ConvLayer(3, 32, kernel_size=9, stride=1)
+        self.in1 = torch.nn.InstanceNorm2d(32, affine=True)
+        self.conv2 = ConvLayer(32, 64, kernel_size=3, stride=2)
+        self.in2 = torch.nn.InstanceNorm2d(64, affine=True)
+        self.conv3 = ConvLayer(64, 128, kernel_size=3, stride=2)
+        self.in3 = torch.nn.InstanceNorm2d(128, affine=True)
+        # Residual layers
+        self.res1 = ResidualBlock(128)
+        self.res2 = ResidualBlock(128)
+        self.res3 = ResidualBlock(128)
+        self.res4 = ResidualBlock(128)
+        self.res5 = ResidualBlock(128)
+        # Upsampling Layers
+        self.deconv1 = UpsampleConvLayer(128, 64, kernel_size=3, stride=1, upsample=2)
+        self.in4 = torch.nn.InstanceNorm2d(64, affine=True)
+        self.deconv2 = UpsampleConvLayer(64, 32, kernel_size=3, stride=1, upsample=2)
+        self.in5 = torch.nn.InstanceNorm2d(32, affine=True)
+        self.deconv3 = ConvLayer(32, 3, kernel_size=9, stride=1)
+        # Non-linearities
+        self.relu = torch.nn.ReLU()
+
+    def forward(self, X):
+        y = self.relu(self.in1(self.conv1(X)))
+        y = self.relu(self.in2(self.conv2(y)))
+        y = self.relu(self.in3(self.conv3(y)))
+        y = self.res1(y)
+        y = self.res2(y)
+        y = self.res3(y)
+        y = self.res4(y)
+        y = self.res5(y)
+        y = self.relu(self.in4(self.deconv1(y)))
+        y = self.relu(self.in5(self.deconv2(y)))
+        y = self.deconv3(y)
+        return y
+
+class TransformerNet2(torch.nn.Module):
+    """ includes two skip connections"""
+    def __init__(self, params):
+        super(TransformerNet2, self).__init__()
+        # Initial convolution layers
+        self.conv1 = ConvLayer(3, 32,
+                               kernel_size = params.conv1_kernel_size1,
+                               stride=1,
+                               extra_padding = params.conv1_extra_padding)
+        self.in1 = torch.nn.InstanceNorm2d(32, affine=True)
+        self.conv2 = ConvLayer(32, 64, kernel_size=3, stride=2)
+        self.in2 = torch.nn.InstanceNorm2d(64, affine=True)
+        #self.conv3 = ConvLayer(64, 128, kernel_size=3, stride=2)
+        #self.in3 = torch.nn.InstanceNorm2d(128, affine=True)
+        # Residual layers
+        self.res1 = ResidualBlock(params.resblock_dim)
+        self.res2 = ResidualBlock(params.resblock_dim)
+        self.res3 = ResidualBlock(params.resblock_dim)
+        #self.res4 = ResidualBlock(params.resblock_dim)
+        #self.res5 = ResidualBlock(128)
+        # Upsampling Layers
+        self.deconv1 = UpsampleConvLayer(params.deconv1_inchannel_size, params.deconv1_outchannel_size, kernel_size=3, stride=1, upsample=2)
+        self.in4 = torch.nn.InstanceNorm2d(params.deconv1_outchannel_size, affine=True)
+        self.deconv2 = UpsampleConvLayer(params.deconv2_inchannel_size+params.input_inchannel_size, params.deconv2_outchannel_size, kernel_size=3, stride=1, upsample=4)
+        self.in5 = torch.nn.InstanceNorm2d(params.deconv2_outchannel_size, affine=True)
+        self.deconv3 = UpsampleConvLayer(params.deconv3_inchannel_size+params.input_inchannel_size, params.deconv3_outchannel_size, kernel_size=3, stride=1, upsample=2)
+        self.in6 = torch.nn.InstanceNorm2d(params.deconv3_outchannel_size, affine=True)
+        self.deconv_out = ConvLayer(params.deconv3_outchannel_size, 3, kernel_size=9, stride=1)
+        # Non-linearities
+        self.relu = torch.nn.ReLU()
+        # final downsampling for the content loss        
+        self.downsample_mode = params.downsample_mode
+        self.downsample_factor = params.downsample_factor
+    
+    def forward(self, X):
+        # straight upsampling of the input by x4
+        X_upsampled_x4 = torch.nn.functional.interpolate(X, mode='nearest', scale_factor=4)
+        # convolution
+        y = self.relu(self.in1(self.conv1(X)))
+        y = self.relu(self.in2(self.conv2(y)))
+        #y = self.relu(self.in3(self.conv3(y)))
+        y = self.res1(y)
+        y = self.res2(y)
+        y = self.res3(y)
+        #y = self.res4(y)
+        #y = self.res5(y)
+        y_back_to_orig_dim = self.relu(self.in4(self.deconv1(y))) # in[32,32] out [64,64]
+        y_cat = torch.cat([X, y_back_to_orig_dim],axis=1)
+        y_dim_x4 = self.relu(self.in5(self.deconv2(y_cat))) # in [64,64] out [256,256]
+        y_cat = torch.cat([X_upsampled_x4, y_dim_x4], axis=1)
+        y_dim_x8 = self.relu(self.in6(self.deconv3(y_cat))) # in [256,256] out[512,512]
+        out = self.deconv_out(y_dim_x8)
+        out_downsampled = torch.nn.functional.interpolate(out, mode=self.downsample_mode, scale_factor=self.downsample_factor)
+        return out, out_downsampled
+
+class TransformerNet3(torch.nn.Module):
+    """ includes just one skip connection"""
+    def __init__(self, params):
+        super(TransformerNet3, self).__init__()
+        # Initial convolution layers
+        self.conv1 = ConvLayer(3, 32,
+                               kernel_size = params.conv1_kernel_size1,
+                               stride=1,
+                               extra_padding = params.conv1_extra_padding)
+        self.in1 = torch.nn.InstanceNorm2d(32, affine=True)
+        self.conv2 = ConvLayer(32, 64, kernel_size=3, stride=2)
+        self.in2 = torch.nn.InstanceNorm2d(64, affine=True)
+        #self.conv3 = ConvLayer(64, 128, kernel_size=3, stride=2)
+        #self.in3 = torch.nn.InstanceNorm2d(128, affine=True)
+        # Residual layers
+        self.res1 = ResidualBlock(params.resblock_dim)
+        self.res2 = ResidualBlock(params.resblock_dim)
+        self.res3 = ResidualBlock(params.resblock_dim)
+        #self.res4 = ResidualBlock(params.resblock_dim)
+        #self.res5 = ResidualBlock(128)
+        # Upsampling Layers
+        self.deconv1 = UpsampleConvLayer(params.deconv1_inchannel_size, params.deconv1_outchannel_size, kernel_size=3, stride=1, upsample=2)
+        self.in4 = torch.nn.InstanceNorm2d(params.deconv1_outchannel_size, affine=True)
+        self.deconv2 = UpsampleConvLayer(params.deconv2_inchannel_size+params.input_inchannel_size, params.deconv2_outchannel_size, kernel_size=3, stride=1, upsample=4)
+        self.in5 = torch.nn.InstanceNorm2d(params.deconv2_outchannel_size, affine=True)
+        #self.deconv3 = UpsampleConvLayer(params.deconv3_inchannel_size+params.input_inchannel_size, params.deconv3_outchannel_size, kernel_size=3, stride=1, upsample=2)
+        self.deconv3 = UpsampleConvLayer(params.deconv3_inchannel_size, params.deconv3_outchannel_size, kernel_size=3, stride=1, upsample=2)
+        self.in6 = torch.nn.InstanceNorm2d(params.deconv3_outchannel_size, affine=True)
+        self.deconv_out = ConvLayer(params.deconv3_outchannel_size, 3, kernel_size=9, stride=1)
+        # Non-linearities
+        self.relu = torch.nn.ReLU()
+        # final downsampling for the content loss        
+        self.downsample_mode = params.downsample_mode
+        self.downsample_factor = params.downsample_factor
+    
+    def forward(self, X):
+        # straight upsampling of the input by x4
+        #X_upsampled_x4 = torch.nn.functional.interpolate(X, mode='nearest', scale_factor=4)
+        # convolution
+        y = self.relu(self.in1(self.conv1(X)))
+        y = self.relu(self.in2(self.conv2(y)))
+        #y = self.relu(self.in3(self.conv3(y)))
+        y = self.res1(y)
+        y = self.res2(y)
+        y = self.res3(y)
+        #y = self.res4(y)
+        #y = self.res5(y)
+        y_back_to_orig_dim = self.relu(self.in4(self.deconv1(y))) # in[32,32] out [64,64]
+        y_cat = torch.cat([X, y_back_to_orig_dim],axis=1)
+        y_dim_x4 = self.relu(self.in5(self.deconv2(y_cat))) # in [64,64] out [256,256]
+        #y_cat = torch.cat([X_upsampled_x4, y_dim_x4], axis=1)
+        y_dim_x8 = self.relu(self.in6(self.deconv3(y_dim_x4))) # in [256,256] out[512,512]
+        out = self.deconv_out(y_dim_x8)
+        out_downsampled = torch.nn.functional.interpolate(out, mode=self.downsample_mode, scale_factor=self.downsample_factor)
+        return out, out_downsampled
+
+class TransformerNet4(torch.nn.Module):
+    """ includes just one skip connection"""
+    def __init__(self, params):
+        super(TransformerNet4, self).__init__()
+        # Initial convolution layers
+        self.conv1 = ConvLayer(3, 32,
+                               kernel_size = params.conv1_kernel_size1,
+                               stride=1,
+                               extra_padding = params.conv1_extra_padding)
+        self.in1 = torch.nn.InstanceNorm2d(32, affine=True)
+        self.conv2 = ConvLayer(32, 64, kernel_size=3, stride=2)
+        self.in2 = torch.nn.InstanceNorm2d(64, affine=True)
+        #self.conv3 = ConvLayer(64, 128, kernel_size=3, stride=2)
+        #self.in3 = torch.nn.InstanceNorm2d(128, affine=True)
+        # Residual layers
+        self.res1 = ResidualBlock(params.resblock_dim)
+        self.res2 = ResidualBlock(params.resblock_dim)
+        self.res3 = ResidualBlock(params.resblock_dim)
+        #self.res4 = ResidualBlock(params.resblock_dim)
+        #self.res5 = ResidualBlock(128)
+        # Upsampling Layers
+        self.deconv1 = UpsampleConvLayer(params.deconv1_inchannel_size, params.deconv1_outchannel_size, kernel_size=3, stride=1, upsample=2)
+        self.in4 = torch.nn.InstanceNorm2d(params.deconv1_outchannel_size, affine=True)
+        self.deconv2 = UpsampleConvLayer(params.deconv2_inchannel_size+params.input_inchannel_size, params.deconv2_outchannel_size, kernel_size=3, stride=1, upsample=4)
+        self.in5 = torch.nn.InstanceNorm2d(params.deconv2_outchannel_size, affine=True)
+        #self.deconv3 = UpsampleConvLayer(params.deconv3_inchannel_size+params.input_inchannel_size, params.deconv3_outchannel_size, kernel_size=3, stride=1, upsample=2)
+        self.deconv3 = UpsampleConvLayer(params.deconv3_inchannel_size, params.deconv3_outchannel_size, kernel_size=3, stride=1, upsample=2)
+        self.in6 = torch.nn.InstanceNorm2d(params.deconv3_outchannel_size, affine=True)
+        self.deconv_out = ConvLayer(params.deconv3_outchannel_size, 3, kernel_size=9, stride=1)
+        # Non-linearities
+        self.relu = torch.nn.ReLU()
+        # final downsampling for the content loss        
+        #self.downsample_mode = params.downsample_mode
+        #self.downsample_factor = params.downsample_factor
+    
+    def forward(self, X):
+        # straight upsampling of the input by x4
+        #X_upsampled_x4 = torch.nn.functional.interpolate(X, mode='nearest', scale_factor=4)
+        # convolution
+        y = self.relu(self.in1(self.conv1(X)))
+        y = self.relu(self.in2(self.conv2(y)))
+        #y = self.relu(self.in3(self.conv3(y)))
+        y = self.res1(y)
+        y = self.res2(y)
+        y = self.res3(y)
+        #y = self.res4(y)
+        #y = self.res5(y)
+        y_back_to_orig_dim = self.relu(self.in4(self.deconv1(y))) # in[32,32] out [64,64]
+        y_cat = torch.cat([X, y_back_to_orig_dim],axis=1)
+        y_dim_x4 = self.relu(self.in5(self.deconv2(y_cat))) # in [64,64] out [256,256]
+        #y_cat = torch.cat([X_upsampled_x4, y_dim_x4], axis=1)
+        y_dim_x8 = self.relu(self.in6(self.deconv3(y_dim_x4))) # in [256,256] out[512,512]
+        out = self.deconv_out(y_dim_x8)
+        #out_downsampled = torch.nn.functional.interpolate(out, mode=self.downsample_mode, scale_factor=self.downsample_factor)
+        return out, None
+
+class ConvLayer(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, extra_padding=None):
+        super(ConvLayer, self).__init__()
+        if extra_padding is None:
+            extra_padding = 0
+        reflection_padding = kernel_size // 2 + extra_padding
+        self.reflection_pad = torch.nn.ReflectionPad2d(reflection_padding)
+        self.conv2d = torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride)
+    
+    def forward(self, x):
+        out = self.reflection_pad(x)
+        out = self.conv2d(out)
+        return out
+
+
+class ResidualBlock(torch.nn.Module):
+    """ResidualBlock
+    introduced in: https://arxiv.org/abs/1512.03385
+    recommended architecture: http://torch.ch/blog/2016/02/04/resnets.html
+    """
+
+    def __init__(self, channels):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = ConvLayer(channels, channels, kernel_size=3, stride=1)
+        self.in1 = torch.nn.InstanceNorm2d(channels, affine=True)
+        self.conv2 = ConvLayer(channels, channels, kernel_size=3, stride=1)
+        self.in2 = torch.nn.InstanceNorm2d(channels, affine=True)
+        self.relu = torch.nn.ReLU()
+
+    def forward(self, x):
+        residual = x
+        out = self.relu(self.in1(self.conv1(x)))
+        out = self.in2(self.conv2(out))
+        out = out + residual
+        return out
+
+
+class UpsampleConvLayer(torch.nn.Module):
+    """UpsampleConvLayer
+    Upsamples the input and then does a convolution. This method gives better results
+    compared to ConvTranspose2d.
+    ref: http://distill.pub/2016/deconv-checkerboard/
+    """
+
+    def __init__(self, in_channels, out_channels, kernel_size, stride, upsample=None):
+        super(UpsampleConvLayer, self).__init__()
+        self.upsample = upsample
+        reflection_padding = kernel_size // 2
+        self.reflection_pad = torch.nn.ReflectionPad2d(reflection_padding)
+        self.conv2d = torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride)
+
+    def forward(self, x):
+        x_in = x
+        if self.upsample:
+            x_in = torch.nn.functional.interpolate(x_in, mode='nearest', scale_factor=self.upsample)
+        out = self.reflection_pad(x_in)
+        out = self.conv2d(out)
+        return out
---
+++ src/train.py
@@ -0,0 +1,274 @@
+import sys
+import time
+import os
+import json
+import pickle as pkl
+
+import torch
+import torchvision.transforms as Transforms
+import albumentations as A
+import cv2
+import random
+
+from utils.utils import rebase_path
+from utils.visualize_image import save_visualization
+from utils.image_transform import pil_loader, ToTensorV2
+from attrib_dataset import AttribDataset
+
+import importlib
+import json
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+from nets.transformer_net import *
+
+from nets.vgg import Vgg16
+
+mse_loss = torch.nn.MSELoss()
+
+# make the image data loader
+class DataLoaderTransformer:
+    def __init__(self,
+                 path_db, # path to database
+                 albumentations_transformations=None,
+                 selectedAttributes = None, # select according to certain attributes
+                 size_compressed_input = None
+                 ):
+        # a wrapper class that invokes an Albumentations Compose, conditional on a 'size' argument
+        self.albumentations_transformations = albumentations_transformations
+        
+        # path to dataset, separated into different class-directories
+        self.path_db = path_db
+        
+        self.selectedAttributes = selectedAttributes
+        
+        # build key order
+        self.attribKeysOrder = self.getDataset(size=10).getKeyOrders()
+        print("AC;classes : ")
+        print(self.attribKeysOrder)
+        
+        #
+        if size_compressed_input is None:
+            size_compressed_input = 64
+            print("setting 'input' image size to %d" % size_compressed_input)
+        self.size_compressed_input = size_compressed_input
+    
+    def getDBLoader(self, size, bs, num_workers = None, shuffle=None):
+        r"""
+        Load the training dataset for the given scale.
+        Args:
+            - scale (int): scale at which we are working
+        Returns:
+            A dataset with properly resized inputs.
+        """
+        if shuffle is None:
+            shuffle =True
+        
+        if num_workers is None:
+            num_workers = 1
+        
+        dataset = self.getDataset(size)
+        
+        return torch.utils.data.DataLoader(dataset, batch_size=bs, shuffle=shuffle, num_workers=num_workers) 
+    
+    def getDataset(self, size=None):
+        
+        if size is None:
+            size = 512
+        
+        print("size", size)
+        if self.albumentations_transformations is None:
+            raise NotImplementedError("need to provide a class of Albumentations Compose called argument 'albumentations_transformations'")
+        
+        transform = self.albumentations_transformations(size)
+        
+        return AttribDataset(self.path_db,
+                             transform=transform,
+                             attribDictPath=None, #self.pathAttribDict,
+                             specificAttrib=self.selectedAttributes,
+                             mimicImageFolder=True)#self.imagefolderDataset)
+    
+    def downscale_fullsize_target_to_small_input(self, target_images, size_compressed_input=None):
+        """ take the target image (generally 512x512) and downscale it to 64x64"""
+        if size_compressed_input is None:
+            size_compressed_input = self.size_compressed_input
+            if size_compressed_input is None:
+                raise AttributeError("'size_compressed_input' is None; must set to ~64")
+        # change to Numpy
+        #target_image = target_image.detach().numpy()
+        return Transforms.Resize(size_compressed_input, interpolation=2)(target_images)
+
+class AlbumentationsTransformations(object):
+    """ Wrapper class used to re-initialize a Albumentations Compose, conditional on changing parameters of 'size' """
+    def __init__(self, size):
+        if isinstance(size,int):
+            size = (size,size)
+        self.A_Compose = A.Compose([A.ChannelShuffle(always_apply=False, p=0.1),
+                                    A.RandomGridShuffle(always_apply=False, p=0.05, grid=(2, 2)),
+                       A.transforms.GridDistortion(num_steps=5, distort_limit=0.4, p=0.6),
+                       A.HorizontalFlip(p=0.5),
+                       A.RandomResizedCrop(always_apply=True, p=1.0, height=size[0], width=size[0],
+                                         scale=(0.75, 1),
+                                         ratio=(0.97, 1.03), interpolation=3),#ONLY 3 SEEMS TO WORK
+                       A.transforms.ColorJitter(brightness=0.05, contrast=0.07, saturation=0.04, hue=0.08, always_apply=False, p=0.7),#HSV random
+                       A.transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)), 
+                       ToTensorV2()
+                       ])
+    
+    def __call__(self, PIL_image):
+        transformed_image_dict = self.A_Compose(image = np.array(PIL_image))
+        return transformed_image_dict['image']
+    
+    def __repr__(self):
+        format_string = self.__class__.__name__ + '('
+        for t in self.transforms:
+            format_string += '\n'
+            format_string += '    {0}'.format(t)
+        format_string += '\n)'
+        return format_string
+
+def gram_matrix(y):
+    """ gram matrix for style loss"""
+    (b, ch, h, w) = y.size()
+    features = y.view(b, ch, w * h)
+    features_t = features.transpose(1, 2)
+    gram = features.bmm(features_t) / (ch * h * w)
+    return gram
+
+def style_lossf(gram_style_fullsize, gram_style_reconstruction):
+    style_loss = 0.
+    # loop through vgg features
+    for gm_o, gm_r in zip(gram_style_fullsize, gram_style_reconstruction):
+        style_loss += mse_loss(gm_o, gm_r)
+    
+    return style_loss 
+
+def train(params_dict,
+          path_db,
+          save_dir,
+          do_cuda = None,
+          do_reload=None,
+          n_epochs = None,
+          bs = None,
+          lr = None,
+          wts = None,
+          path_to_reload_model = None,
+          sleep=None):
+    
+    if do_cuda is None:
+        do_cuda = False
+    
+    mse_loss = torch.nn.MSELoss()
+    
+    size_compressed_input = (64,64)
+    
+    # initialize the db loader class
+    db_loader = DataLoaderTransformer(
+        path_db = path_db, #'/tmp/data/', 
+        albumentations_transformations=AlbumentationsTransformations,
+        selectedAttributes=None,
+        size_compressed_input = size_compressed_input)
+    
+    params = HyperParams(params_dict)
+    
+    device = torch.device("cuda" if do_cuda else "cpu")
+    
+    vgg = Vgg16(requires_grad=False).to(device)
+    
+    net2 = TransformerNet4(params).to(device)
+    optimizer = torch.optim.Adam(net2.parameters(),lr = lr)
+    
+    save_dir = "models_trained/v2/";
+    if not os.path.isdir(save_dir):
+        os.mkdir(save_dir)
+    
+    target_size = 512
+    
+    if n_epochs is None:
+        n_epochs = 10
+    
+    if bs is None:
+        bs = 3
+    
+    if lr is None:
+        lr = 0.001
+    
+    if wts is None:
+        wts = {'style':1e10, 'pixel':512.0**-0.5, 'content':1e5}
+    
+    epoch = -1
+    
+    if do_reload is None:
+        do_reload = False
+    
+    if do_reload:
+        if os.path.isfile(path_to_reload_model):
+            print("reloading model %s" % path_to_reload_model)
+            previous_model_state = torch.load(path_to_reload_model)
+            epoch = previous_model_state['epoch']
+            print("resuming at epoch %d" % epoch)
+            net2.load_state_dict(previous_model_state['model_state_dict'])
+            optimizer = torch.optim.Adam(net2.parameters(),lr = lr)
+            optimizer.load_state_dict(previous_model_state['optimizer_state_dict'])
+    
+    while epoch < n_epochs:
+        epoch+=1
+        db_iter = db_loader.getDBLoader(size=target_size, bs=bs, num_workers = 1, shuffle=True)
+        # loop through the images
+        for bitem, (by_full, blabels) in enumerate(db_iter):
+            
+            # compress the image to 64x64  (serves as the input image
+            bx_comp = db_loader.downscale_fullsize_target_to_small_input(target_images = by_full)
+            optimizer.zero_grad()
+            # reconstruct image
+            out, out_downsampled = net2(bx_comp)
+            # VGG features
+            features_style_fullsize = vgg(by_full)
+            features_style_reconstruction = vgg(out)
+        
+            # gram matrices for style loss
+            gram_style_fullsize = [gram_matrix(y) for y in features_style_fullsize]
+            # style of the reconstruction
+            gram_style_reconstruction = [gram_matrix(y) for y in features_style_reconstruction]
+            # style loss
+            bstyle_loss = style_lossf(gram_style_fullsize, gram_style_reconstruction)
+            
+            # pixel loss
+            pixel_loss = mse_loss(by_full, out)
+            # content loss (vgg deeper features
+            #content_loss = mse_loss(bx_comp, out_downsampled)
+            content_loss = mse_loss(features_style_fullsize.relu2_2, features_style_reconstruction.relu2_2)
+            
+            #bstyle_loss.backward() # takes a long time
+            rwts_pixel = wts['pixel']*(random.random()<0.95);
+            #rwts_content = wts['content']*(random.random()<0.95)
+            (rwts_pixel*pixel_loss + wts['style']*bstyle_loss + wts['content']*content_loss).backward()
+            optimizer.step()
+            if bitem % 10 ==0:
+                print("E%d; STEP%d; style:%0.6f; content:%0.3f; pix:%0.3f" % (epoch, bitem, bstyle_loss.item(), content_loss.item(), pixel_loss.item()))
+                if not sleep is None:
+                    time.sleep(sleep)
+          
+        if True:
+            print("saving model and making a dummy image")
+            path_to_model = "%sstyletransfer_v1_e%d.model" % (save_dir, epoch)
+            torch.save({
+                'epoch': epoch,
+                'model_state_dict': net2.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'style_loss':bstyle_loss.item(),
+                'content_loss':content_loss.item(),
+                'pixel_loss':pixel_loss.item(),
+                'lr':lr,
+                'weights':wts,
+                'bs':bs,
+                'params_dict':params_dict},path_to_model)
+            
+            # plot images
+            save_visualization(by_full, out, vis_path=save_dir, suffix = "E%d" % epoch)
+            if not sleep is None:
+                time.sleep(5*sleep)
+
+    
